a35758fed0bfe815c54705a35c347a1d2f772c22
cicodes <35441543+cicodes@users.noreply.github.com>
2021-03-10 19:15:08 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,13 @@
-#Explain your work
+ #Create two lists
+a = list(range(1,10,2))
+b = list(range(0,10,2))
 
-#Question 1
-for x in range(a):
-	print(a)
+#Merge a,b lists
+newList = a+b
+
+#Multiple all values in the newList by 2 
+newList = [i*2 for i in newList]
+
+#Print the data type of all values in new list
+for item in newList:
+    print(type(item))
